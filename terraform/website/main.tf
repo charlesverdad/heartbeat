9c6238a61d88a0e9208617379477b2773020c8eb
Charles Verdad <charles.verdad@gmail.com>
2025-07-09 14:38:16 +1000

    fix and apply to dev

--- terraform/website/main.tf
+++ terraform/website/main.tf
@@ -23,6 +23,9 @@ resource "azurerm_key_vault" "main" {
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
 
+  # Enable RBAC authorization instead of access policies
+  enable_rbac_authorization = true
+
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
 
@@ -49,9 +52,109 @@ resource "azurerm_role_assignment" "keyvault_secrets_user" {
   principal_id         = azurerm_user_assigned_identity.main.principal_id
 }
 
+# Role assignment for current user to manage secrets during deployment
+resource "azurerm_role_assignment" "current_user_keyvault_admin" {
+  scope                = azurerm_key_vault.main.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
 # Role assignment for AKS kubelet to use this identity (reference from AKS module)
 resource "azurerm_role_assignment" "aks_identity_operator" {
   scope                = azurerm_user_assigned_identity.main.id
   role_definition_name = "Managed Identity Operator"
   principal_id         = var.aks_kubelet_identity_object_id
+}
+
+# Generate random password for MySQL
+resource "random_password" "mysql_admin" {
+  length  = 16
+  special = true
+}
+
+# Azure Database for MySQL - Flexible Server (cost-effective)
+resource "azurerm_mysql_flexible_server" "main" {
+  name                   = "mysql-${var.project_name}-${var.environment}-${random_string.suffix.result}"
+  resource_group_name    = azurerm_resource_group.main.name
+  location               = azurerm_resource_group.main.location
+  
+  # Minimal configuration for cost savings
+  administrator_login    = "ghostadmin"
+  administrator_password = random_password.mysql_admin.result
+  
+  # Cheapest tier available
+  sku_name = "B_Standard_B1ms"  # Burstable, 1 vCore, 2GB RAM
+  
+  # Minimal storage (20GB minimum)
+  storage {
+    size_gb = 20
+    iops    = 360  # Minimum for this tier
+  }
+  
+  # MySQL version
+  version = "8.0.21"
+  
+  # Availability zone (set to match existing)
+  zone = "1"
+  
+  # Backup configuration - weekly to save costs
+  backup_retention_days = 7
+  
+  # High availability disabled for cost savings (omit the block to disable)
+  # high_availability {
+  #   mode = "ZoneRedundant"  # Only ZoneRedundant or SameZone allowed
+  # }
+  
+  # Network access is determined automatically
+  
+  tags = var.tags
+}
+
+# Firewall rule to allow Azure services
+resource "azurerm_mysql_flexible_server_firewall_rule" "allow_azure_services" {
+  name                = "AllowAzureServices"
+  resource_group_name = azurerm_resource_group.main.name
+  server_name         = azurerm_mysql_flexible_server.main.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
+
+# Create Ghost database
+resource "azurerm_mysql_flexible_database" "ghost" {
+  name                = "ghost"
+  resource_group_name = azurerm_resource_group.main.name
+  server_name         = azurerm_mysql_flexible_server.main.name
+  charset             = "utf8mb4"
+  collation           = "utf8mb4_unicode_ci"
+}
+
+# Store MySQL connection details in Key Vault
+resource "azurerm_key_vault_secret" "mysql_connection_string" {
+  name         = "mysql-connection-string"
+  value        = "mysql://${azurerm_mysql_flexible_server.main.administrator_login}:${random_password.mysql_admin.result}@${azurerm_mysql_flexible_server.main.fqdn}:3306/${azurerm_mysql_flexible_database.ghost.name}?ssl=true"
+  key_vault_id = azurerm_key_vault.main.id
+  
+  depends_on = [
+    azurerm_role_assignment.keyvault_secrets_user
+  ]
+}
+
+resource "azurerm_key_vault_secret" "mysql_password" {
+  name         = "mysql-password"
+  value        = random_password.mysql_admin.result
+  key_vault_id = azurerm_key_vault.main.id
+  
+  depends_on = [
+    azurerm_role_assignment.keyvault_secrets_user
+  ]
+}
+
+resource "azurerm_key_vault_secret" "mysql_host" {
+  name         = "mysql-host"
+  value        = azurerm_mysql_flexible_server.main.fqdn
+  key_vault_id = azurerm_key_vault.main.id
+  
+  depends_on = [
+    azurerm_role_assignment.keyvault_secrets_user
+  ]
 }
--- terraform/website/outputs.tf
+++ terraform/website/outputs.tf
@@ -22,3 +22,23 @@ output "managed_identity_principal_id" {
   description = "Principal ID of the managed identity"
   value       = azurerm_user_assigned_identity.main.principal_id
 }
+
+output "mysql_server_name" {
+  description = "Name of the MySQL server"
+  value       = azurerm_mysql_flexible_server.main.name
+}
+
+output "mysql_server_fqdn" {
+  description = "FQDN of the MySQL server"
+  value       = azurerm_mysql_flexible_server.main.fqdn
+}
+
+output "mysql_database_name" {
+  description = "Name of the Ghost database"
+  value       = azurerm_mysql_flexible_database.ghost.name
+}
+
+output "mysql_admin_username" {
+  description = "MySQL administrator username"
+  value       = azurerm_mysql_flexible_server.main.administrator_login
+}
