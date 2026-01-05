7b98b7e87d9681b4f30bccfdb8324d91012221ba
Charles Verdad <charles.verdad@gmail.com>
2026-01-05 19:42:09 +1100

    remove unused folder living-life-quiz

--- terraform/website-living-life-quiz/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state-backend"
-    storage_account_name = "terraforminfratfstate"
-    container_name       = "tfstate"
-    key                  = "website-living-life-quiz/terraform.tfstate"
-    use_azuread_auth     = true
-  }
-}
